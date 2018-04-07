e0f179570064773e0d13e0fb18117248ad9fdf29
Espen Henriksen <dev@henriksen.is>
2018-04-07 22:30:48 +0200

    Add head, method and header options

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ extern crate hyper;
 extern crate hyper_tls;
 extern crate tokio_core;
 
-use hyper::{Client, Uri};
+use std::process::exit;
+use hyper::{Client, Method, Request, Uri};
 use hyper::client::Response;
 use hyper::header::SetCookie;
 use hyper_tls::HttpsConnector;
@@ -17,14 +18,31 @@ fn main() {
     let matches = App::from_yaml(yaml).get_matches();
 
     let url = value_t_or_exit!(matches, "url", Uri);
+    let headers = values_t!(matches, "headers", String).unwrap_or(Vec::new());
+    let method = value_t!(matches, "method", Method).unwrap_or(Method::Get);
+    let print_headers = matches.is_present("head");
+    let request = build_request(url, method, headers);
 
-    match fetch(url) {
+    match fetch(request, print_headers) {
         Ok(mut response) => {
-            let cookies = response.headers_mut().get::<SetCookie>().unwrap();
+            if print_headers {
+                eprintln!("Authentication response");
+                eprintln!("---");
+                eprintln!("{} {}", response.version(), response.status());
+                eprintln!("{}", response.headers());
+            }
+
+            let cookies = response
+                .headers_mut()
+                .get::<SetCookie>()
+                .unwrap_or_else(|| {
+                    eprintln!("No Set-Cookie header present");
+                    exit(1);
+                });
 
             let mut cookie_values = Vec::new();
             for cookie in cookies.iter() {
-                // Get only key=value part of cookie
+                // Get only key=value part of cookie, not the metadata
                 let split = cookie.split(';').collect::<Vec<&str>>();
                 if let Some(header) = split.first() {
                     cookie_values.push(header.clone());
@@ -37,11 +55,47 @@ fn main() {
     }
 }
 
-fn fetch(uri: Uri) -> Result<Response, hyper::Error> {
+fn build_request(uri: Uri, method: Method, headers: Vec<String>) -> Request {
+    let mut request = Request::new(method.clone(), uri.clone());
+
+    for header in headers.iter() {
+        let split = header.split(':').collect::<Vec<&str>>();
+        request
+            .headers_mut()
+            .set_raw(split[0].to_owned(), split[1].to_owned());
+    }
+
+    request
+}
+
+fn fetch(request: Request, print_headers: bool) -> Result<Response, hyper::Error> {
     let mut core = Core::new()?;
+
+    if print_headers {
+        eprintln!("Authentication request");
+        eprintln!("---");
+        if let Some(query) = request.uri().query() {
+            eprintln!(
+                "{} {}?{} {}",
+                request.method(),
+                request.uri().path(),
+                query,
+                request.version()
+            );
+        } else {
+            eprintln!(
+                "{} {} {}",
+                request.method(),
+                request.uri().path(),
+                request.version()
+            );
+        }
+        eprintln!("{}", request.headers());
+    }
+
     let client = Client::configure()
         .connector(HttpsConnector::new(1, &core.handle()).unwrap())
         .build(&core.handle());
 
-    core.run(client.get(uri))
+    core.run(client.request(request))
 }
